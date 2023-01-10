1b27dd85454663289a9d4c8ab97cb6afd63d5e66
Felipe Fernandez <FelipeFelizx@gmail.com>
2023-01-10 03:15:58 -0300

    Fix: Cambio de API ahora tengo categoria

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -1,124 +1,95 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import '../Styles/Page.css'
 
 const Page = () => {
-
-    const [quotes, setQuotes] = useState(null);
-    const [gender, setGender] = useState(null);
-    const [selectedAuthor, setSelectedAuthor] = useState(null);
+    const [quote, setQuote] = useState(null);
+    const [category, setCategory] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-    const [randomIndex, setRandomIndex] = useState(0);
 
     /*
     * Obtiene una lista de citas de una API externa y establece el estado de la aplicación en la lista de citas obtenidas.
     */
-    const fetchQuotes = async () => {
-        const result = await axios.get('https://type.fit/api/quotes');
-        setQuotes(result.data);
-        setGender(result.data.gender);
-        setIsLoading(false);
-    };
-
+    const fetchQuote = async () => {
+        const response = await fetch('https://favqs.com/api/qotd', {
+            method: 'GET',
+            headers: {
+                Authorization: 'Token token="73a567c9eaa68890d677caf1d367e1db"',
+            },
+        });
+        const data = await response.json();
+        setCategory(data.quote.tags);
+        return data.quote;
+    }; 
     /*
     * useeffect que se encarga de obtener las citas de la API cuando se carga la página.
     * @param {function} fetchQuotes - Función que obtiene las citas de la API.
     * @returns {undefined}
     */
     useEffect(() => {
-        fetchQuotes();
+        fetchQuote().then(quote => {
+            setQuote(quote);
+            setIsLoading(false);
+        });
     }, []);
-
-    /*
-    * Efecto de lado que establece un índice aleatorio y el autor seleccionado en "null" cuando se actualiza la lista de citas.
-    *
-    * @param {array} quotes - Lista de citas.
-    * @param {function} setRandomIndex - Función que establece el índice aleatorio en el estado de la aplicación.
-    * @param {function} setSelectedAuthor - Función que establece el autor seleccionado en el estado de la aplicación.
-    * @returns {undefined}
-    */
-    useEffect(() => {
-        if (quotes) {
-            const randomIndex = Math.floor(Math.random() * quotes.length);
-            setRandomIndex(randomIndex);
-            setSelectedAuthor(null);
-        }
-    }, [quotes]);
 
     /* Muestra todas las citas del autor especificado.
     *
     * @param { string } author - Nombre del autor cuyas citas se deben mostrar.
     * @returns { undefined }
     */
-    const showAllQuotes = (author) => {
-        setSelectedAuthor(author);
+    const showQuotesByAuthor = async (author) => {
+        const response = await fetch(`https://favqs.com/api/quotes/?filter=${author}&type=author`, {
+            method: 'GET',
+            headers: {
+                Authorization: 'Token token="73a567c9eaa68890d677caf1d367e1db"'
+            },
+        })
+        const data = await response.json()
+        setQuote(data.quotes)
+    }
+
+    const showRandomQuote = async () => {
+        const response = await fetch('https://favqs.com/api/qotd', {
+            method: 'GET',
+            headers: {
+                Authorization: 'Token token="73a567c9eaa68890d677caf1d367e1db"',
+            },
+        });
+        const data = await response.json();
+        console.log(data);  // imprime la respuesta de la API
+        setQuote(data.quote);
     };
 
-    /*
-    * Muestra una cita aleatoria y establece el autor seleccionado en "null".
-    * @param {array} quotes - Lista de citas.
-    * @param {function} setRandomIndex - Función que establece el índice aleatorio en el estado de la aplicación.
-    * @param {function} setSelectedAuthor - Función que establece el autor seleccionado en el estado de la aplicación.
-    * @returns {undefined}
-    */
-    const showRandomQuote = () => {
-        if (quotes) {
-            const randomIndex = Math.floor(Math.random() * quotes.length);
-            setRandomIndex(randomIndex);
-            setSelectedAuthor(null);
-        }
-    };
 
-    //mostrar el genero de la cita
 
-    return (
+   return(
         <div className="container">
             <h2>Get your daily quote</h2>
             {isLoading ? (
-                <>
-                    <div className="quoteContainer">
-                        <div className="quoteText">
-                            <p className="quoteText">
-                                Loading...
-                            </p>
-                        </div>
-                        <p className="quoteAuthor">
-                            Loading...
-                        </p>
+                <div className="quoteContainer">
+                    <div className="quoteText">
+                        <p className="quoteText">Loading...</p>
                     </div>
-
-                </>
+                    <p className="quoteAuthor">Loading...</p>
+                </div>
             ) : (
-                <div>
-                    {selectedAuthor ? (
-                        quotes.filter(q => q.author === selectedAuthor).map(quote => (
-                            <div className="quoteContainer" key={quote.id}>
-                                <div className="quoteText">
-                                    <p className="quoteText">
-                                        {quote.text}
-                                    </p>
-                                </div>
-                                <p className="quoteAuthor">
-                                    {quote.author}
-                                </p>
-                            </div>
-                        ))
-                    ) : (
-                        <div className="quoteContainer">
-                            <div className="quoteText">
-                                <p className="quoteText">
-                                    {quotes[randomIndex].text}
-                                </p>
-                            </div>
-                            <p className="quoteAuthor">
-                                <button className="quoteAuthor" value='All Quotes' onClick={() => showAllQuotes(quotes[randomIndex].author)}>
-                                            {quotes[randomIndex].author} - ({quotes.gender || 'gender unknown'})
-                                </button>
-                            </p>
-                        </div>
-                    )}
+                <div className="quoteContainer">
+                    <div className="quoteText">
+                        <p className="quoteText">"{quote.body}"</p>
+                    </div>
+                    {/* autor */}
+                    <p className="quoteAuthor">- {quote.author}</p>
+                       {/* Categoría */}
+                       <p className="quoteCategory">Category: {category.join(", ")}</p>
                 </div>
             )}
+
+           {/* Boton para ver todas las citas del autor */}
+           <div className="buttonContainer">
+               <a className="quoteButton" onClick={() => showQuotesByAuthor(quote.author)}>See all quotes by {quote.author}</a>
+           </div>
+
+            {/* Boton para pedir otra cita */}
             <div className="buttonContainer">
                 <a className="quoteButton" onClick={showRandomQuote}>See another quote</a>
             </div>
